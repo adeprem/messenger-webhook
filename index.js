df7b72703952953ec47caa535499e9bb91020a39
Auriane de Premilhat <adeprem@vt.edu>
2018-05-13 17:16:14 +0200

    trying to fix the GET error on heroku

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const
   express = require('express'),
   bodyParser = require('body-parser'),
   //app = express().use(bodyParser.json()); // creates express http server
-  app = express().use(express.static(__dirname + '/')),
+  app = express().use(express.static(__dirname + '/'));
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
